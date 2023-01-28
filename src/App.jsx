7df6d1fb71d7d3be92d7309bf3a12f65a4d566ee
Alessandro Camargo <acamargodias@terra.com.br>
2023-01-28 08:19:24 -0300

    feat: finalização projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
 import "./App.css";
+import List from "./components/List";
+import { useState } from "react";
+import data from "./data";
 
 function App() {
-  return <div className="App"></div>;
+  const [birthdayList, setBirthdayList] = useState(data);
+
+  return (
+    <main>
+      <section className="container">
+        <h3>Front Beginners</h3>
+        <h3>
+          {birthdayList.length > 1
+            ? birthdayList.length + "aniversários hoje"
+            : birthdayList.length + "aniversário hoje"}
+        </h3>
+        <List birthdayList={birthdayList} />
+        <button onClick={() => setBirthdayList([])}>Limpar Lista</button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
---
+++ src/components/List.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function List({ birthdayList }) {
+  return (
+    <>
+      {birthdayList.map((person) => {
+        const { id, name, age, image } = person;
+        return (
+          <article className="person" key={id}>
+            <img src={image} alt="{name}" />
+            <div>
+              <h4>{name}</h4>
+              <p>{age}</p>
+            </div>
+          </article>
+        );
+      })}
+    </>
+  );
+}
+
+export default List;
